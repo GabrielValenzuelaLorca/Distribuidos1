0989cb4a8955c82133a24d8d362fbdb3e869ee91
Drogabito <gabriel.valenzuel.14@sansano.usm.cl>
2017-10-22 01:09:21 -0300

    Crear servidor local

---
+++ src/CentralServer/Conector.java
@@ -0,0 +1,28 @@
+package CentralServer;
+import java.net.*;
+import java.io.*;
+
+public class Conector {
+    ServerSocket server;
+    Socket socket;
+    int puerto=9000;
+    DataOutputStream salida;
+    BufferedReader entrada;
+
+    public void iniciar(){
+        try{
+            server = new ServerSocket(puerto);
+            socket = new Socket();
+            socket = server.accept();
+            entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String mensaje = entrada.readLine();
+            System.out.println(mensaje);
+            salida = new DataOutputStream(socket.getOutputStream());
+            salida.writeUTF("Good Dog");
+            socket.close();
+        }catch (Exception e){
+
+        }
+
+    }
+}
---
+++ src/CentralServer/Main.java
@@ -0,0 +1,9 @@
+package CentralServer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Conector c = new Conector();
+        c.iniciar();
+    }
+}
